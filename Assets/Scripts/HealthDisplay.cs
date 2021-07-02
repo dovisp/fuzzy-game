b808d1dd932b636d527f645c37eded43066dfb96
dovisp <dovispavarde@gmail.com>
2021-07-02 16:28:09 +0300

    Update:Health/Resolution
    Resolution doesn't work right, but it is implemented.

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+   private int health = 100;
+   public Text healthText;
+   
+   
+   
+   // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+   healthText.text = health.ToString();
+
+   
+   
+    healthText.text ="HP: " + health;
+    if(Input.GetKeyDown(KeyCode.Space)){
+    health--;
+}
+
+
+    }
+}
